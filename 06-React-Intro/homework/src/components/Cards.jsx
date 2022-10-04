e0768a331a35ead8b8561af73a1c16078a1a4f47
JuanMaCarrearas <manuucarreras@gmail.com>
2022-10-04 11:09:38 -0300

    terminado

--- 06-React-Intro/homework/src/components/Cards.jsx
+++ 06-React-Intro/homework/src/components/Cards.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import Card from './Card';
 
-export default function Cards(props) {
+export default function Cards({cities}) {
   // acá va tu código
   // tip, podés usar un map
-  return <div>Cards Component</div>
+  return <div>
+              {
+                cities && cities.map(obj =>(
+                  <Card
+                    key = {obj.id}
+                    max = {obj.main.temp_max}
+                    min = {obj.main.temp_min}
+                    name = {obj.name}
+                    img = {obj.weather[0].icon}
+                    onClose ={() => alert(obj.name)}
+
+                  />
+                ))
+              }
+
+         </div>
 };
